9a3637fcbf678024c4939ae3293150b0571f566d
cx2219 <cx2219@maxwell.poly.edu>
2025-07-14 18:18:33 -0400

    make some demo

---
+++ env.sh
@@ -0,0 +1,12 @@
+conda install conda-forge::trimesh
+conda install conda-forge::meshplot
+conda install conda-forge::omegaconf
+conda install conda-forge::warp-lang=*=*cuda126*
+conda install conda-forge::tqdm
+conda install scikit-image
+pip3 install open3d
+pip3 install pymeshlab
+pip3 install PyMCubes
+pip3 install embreex
+
+conda install -c conda-forge pythreejs 
